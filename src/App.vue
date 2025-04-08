5fae8366e6b77d84e26b48064bb161375a9fa4e7
choichangyun <tuffhrgha704@naver.com>
2025-04-08 10:27:55 +0900

    feature Filtering

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <FilteringComponent />
+  </div>
+</template>
+
+<script>
+import FilteringComponent from './views/FilteringComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FilteringComponent,
+  },
+}
+</script>
+
+<style></style>
