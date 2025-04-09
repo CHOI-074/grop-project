3924f9a6cc5542eb26e7fc3b0786a67c0bc3ff64
lee030327 <lee030327@naver.com>
2025-04-09 10:45:39 +0900

    홈 경로 재설정

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css' // Tailwind를 불러오는 부분
+import router from './router'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '../views/Main.vue'
+import NotFound from '../views/NotFound.vue'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'MainPage',
+    component: MainPage,
+  },
+  {
+    path: '/not-found',
+    name: 'NotFound',
+    component: NotFound,
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound,
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
